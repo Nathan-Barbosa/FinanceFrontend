c3327ff9132b08452d6b3f117b56845161015a63
Nathan-Barbosa <nathan.s.barbosa0@gmail.com>
2026-03-27 16:17:06 -0300

    feat: add toggle sidebar with open/close transitions and icon

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,13 @@
 import { ROUTES } from "@/constants";
 import { NavLink } from "react-router-dom";
-import { CaretLeftIcon, ChartBarIcon } from "@phosphor-icons/react";
+import { CaretLeftIcon, ChartBarIcon, ListIcon } from "@phosphor-icons/react";
 import imglogo from "../../assets/finances-logo.png";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
   const navItems = [
     {
       to: ROUTES.HOME,
@@ -13,25 +17,34 @@ const Sidebar = () => {
   ];
 
   return (
-    <aside className="bg-blue-400 text-white h-screen p-4 w-60">
-      <div className="flex justify-end mb-2">
-        <button className="text-black hover:text-white">
-          {<CaretLeftIcon size={24} />}
+    <aside
+      className={`bg-blue-400 text-white h-screen p-4 ${isOpen ? "w-60" : "w-20"} transition-all duration-300`}
+    >
+      <div className={`flex ${isOpen ? "justify-end" : "justify-center"} mb-2`}>
+        <button
+          onClick={toggleSidebar}
+          className="text-black hover:text-white transition"
+        >
+          {isOpen ? <CaretLeftIcon size={24} /> : <ListIcon size={24} />}
         </button>
       </div>
 
-      <div className="flex justify-center mb-4">
-        <img src={imglogo} alt="Logo financias" className="h-20 w-auto" />
-      </div>
+      {isOpen && (
+        <div className="flex justify-center mb-4">
+          <img src={imglogo} alt="Logo financias" className="h-20 w-auto" />
+        </div>
+      )}
 
       <nav>
-        <ul className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-2 text-black hover:text-white">
           {navItems.map(({ to, label, icon }) => (
             <li key={to}>
               <NavLink to={to} className="block p-2 rounded bg-blue-500">
-                <div className="flex gap-2 items-center text-black hover:text-white">
+                <div
+                  className={`flex gap-2 ${isOpen ? "justify-start" : "justify-center"}`}
+                >
                   {icon}
-                  {label}
+                  {isOpen && <span>{label}</span>}
                 </div>
               </NavLink>
             </li>
